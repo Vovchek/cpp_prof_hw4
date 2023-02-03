aba621390997613710c7b15ab342d20ad9d56f51
Vovchek <39309092+Vovchek@users.noreply.github.com>
2023-02-03 07:23:25 +0300

    change print_ip_test

--- print_ip_test.cpp
+++ print_ip_test.cpp
@@ -5,31 +5,43 @@
 
 TEST(PrintIpTest, Test1)
 {
-  
   std::ostringstream ss;
-
   print_ip(int8_t{-1}, ss);
   EXPECT_STREQ(ss.str().c_str(), "255") << "int8_t{-1}";
-  ss.rdbuf()->str("");
+}
+TEST(PrintIpTest, Test2)
+{
+  std::ostringstream ss;
   print_ip(int16_t{0}, ss);
   EXPECT_STREQ(ss.str().c_str(), "0.0") << "int16_t";
-  ss.rdbuf()->str("");
+}
+TEST(PrintIpTest, Test3)
+{
   print_ip(int32_t{2130706433});
   EXPECT_STREQ(ss.str().c_str(), "127.0.0.1") << "int32_t";
-  ss.rdbuf()->str("");
+}
+TEST(PrintIpTest, Test4)
+{
   print_ip(int64_t{8875824491850138409});
   EXPECT_STREQ(ss.str().c_str(), "123.45.67.89.101.112.131.41") << "int64_t";
-  ss.rdbuf()->str("");
+}
+TEST(PrintIpTest, Test5)
+{
   print_ip(std::string{"Hello, World !"});
   EXPECT_STREQ(ss.str().c_str(), "Hello, World!") << "std::string";
-  ss.rdbuf()->str("");
+}
+TEST(PrintIpTest, Test6)
+{
   print_ip(std::vector<int>{100, 200, 300, 400});
   EXPECT_STREQ(ss.str().c_str(), "100.200.300.400") << "std::vector<int>";
-  ss.rdbuf()->str("");
+}
+TEST(PrintIpTest, Test7)
+{
   print_ip(std::list<short>{400, 300, 200, 100});
   EXPECT_STREQ(ss.str().c_str(), "400.300.200.100") << "std::list<short>";
-  ss.rdbuf()->str("");
+}
+TEST(PrintIpTest, Test8)
+{
   print_ip(std::make_tuple(123, 456, 789, 0));
   EXPECT_STREQ(ss.str().c_str(), "123.456.789.0") << "std::tuple<>";
-
 }
